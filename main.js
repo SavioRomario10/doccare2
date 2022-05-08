ef34ef1f7c7ca263203e906db88fbcf69cc897a9
SavioRomario10 <105159278+SavioRomario10@users.noreply.github.com>
2022-05-08 10:55:55 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,61 @@
+window.addEventListener('scroll', onScroll)
+
+onScroll()
+
+function onScroll(){
+    showNavOnScroll()
+    showBackToTopButtonOnScroll()
+    activateMenuAtCurrentSection(home)
+    activateMenuAtCurrentSection(about)
+    activateMenuAtCurrentSection(services)
+    activateMenuAtCurrentSection(contact)
+}
+function activateMenuAtCurrentSection(section){
+    const targetLine = scrollY + innerHeight / 2
+
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+
+    const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
+
+    const sctionEndsAt = sectionTop + sectionHeight
+
+    const sectionEndPassedTargetLine = sctionEndsAt <= targetLine
+
+    const sectionBoundaries = sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
+
+    const sectionId = section.getAttribute('id')
+    const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+    menuElement.classList.remove('active')
+    if(sectionBoundaries){
+        menuElement.classList.add('active')
+    }
+}
+function showNavOnScroll(){
+    if(scrollY > 0){
+        navigation.classList.add('scroll')
+    }
+    else{
+        navigation.classList.remove('scroll')
+    }
+}
+function showBackToTopButtonOnScroll(){
+    if(scrollY > 750){
+        backToTopButton.classList.add('show')
+    }
+    else{
+        backToTopButton.classList.remove('show')
+    }
+}
+function openMenu(){
+    document.body.classList.add('menu-expanded')
+}
+function closeMenu(){
+    document.body.classList.remove('menu-expanded')
+}
+ScrollReveal({
+    origin:'botton',
+    distance: '30px',
+    duration: 700,
+}).reveal('#home, #home img, #home .stats, #services, #services header, #services .cards, #about, #about header, #about .content, #contact');
